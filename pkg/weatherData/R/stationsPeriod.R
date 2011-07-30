633d232a039782f8e32827342050d83df0ba25c8
jacobvanetten (Dr ) <jacobvanetten@yahoo.com>
2011-07-30 19:16:22 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cropcc@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/weatherData/R/stationsPeriod.R
+++ pkg/weatherData/R/stationsPeriod.R
@@ -1,9 +1,10 @@
-stationsPeriod <-
-function(startDate, endDate, stations=data(stations), wholePeriod=FALSE)
+stationsPeriod <- function(startDate, endDate, stations, wholePeriod=FALSE)
 {
-if(startDate >= endDate) stop("startDate before or equal to endDate")
-if(!wholePeriod) stations <- stations[stations$BEGIN <= endDate & stations$END >= startDate,]
-if(wholePeriod) stations <- stations[stations$BEGIN <= startDate & stations$END >= endDate,]
-return(stations)
+	startDate <- as.Date(startDate)
+	endDate <- as.Date(endDate)
+	if(startDate >= endDate) stop("startDate before or equal to endDate")
+	if(!wholePeriod) stations <- stations[as.Date(stations$BEGIN) <= endDate & as.Date(stations$END) >= startDate,]
+	if(wholePeriod) stations <- stations[as.Date(stations$BEGIN) <= startDate & as.Date(stations$END) >= endDate,]
+	return(stations)
 }
 
