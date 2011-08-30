9ad6d6eade7cacab4f162706cccd492d3b70aece
jacobvanetten (Dr ) <jacobvanetten@yahoo.com>
2011-08-30 06:39:02 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cropcc@41 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/weatherData/R/downloadGSOD.R
+++ pkg/weatherData/R/downloadGSOD.R
@@ -73,3 +73,64 @@ function(startYear, endYear, stations, silent=TRUE, tries=2, localdir=NULL)
 	return(files)
 	
 }
+
+listFilesGSOD <-
+function(startYear, endYear, stations, localdir=NULL)
+{
+	startDate <- as.Date(paste(startYear,"-01-01",sep=""))
+	endDate <- as.Date(paste(endYear,"-12-31",sep=""))
+
+	stations <- stationsPeriod(startDate, endDate, stations)
+
+	files <- NULL
+
+	#loop by station
+
+	for(station in 1:length(stations[,1]))
+	{
+
+		#set up things for the station
+		startYearStation <- substring(as.Date(stations$BEGIN[station]),1,4)
+		endYearStation <- substring(as.Date(stations$END[station]),1,4)
+		START <- max(startYear, startYearStation)
+		END <- min(endYear, endYearStation)
+
+		#loop through years for the station
+
+		absent <- TRUE
+
+		for (year in START:END)
+		{
+			if(!is.null(localdir))
+			{
+				tryit <- try(file(paste(localdir, "/", "gosd-", stations[station, 2], "-", stations[station, 3], "-", year, ".op.gz", sep = "")))
+				if(class(tryit) != "try-error") 
+				{
+					absent <- FALSE
+				}
+			}
+
+			if(is.null(localdir) | absent)
+			{
+	
+				#set things up -- url and file name, counter for tries
+
+				urlYearStation <- paste("ftp://ftp.ncdc.noaa.gov/pub/data/gsod/", year, "/", stations[station, 2], "-", stations[station, 3], "-", year, ".op.gz", sep = "")
+
+				#add file name to final file list
+
+				files <- c(files, urlYearStation)
+
+
+			}
+
+			absent <- TRUE
+
+		}
+
+
+	}
+	
+	return(files)
+	
+}
