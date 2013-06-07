2f32697d2396cdbf1721b98afda7c0dd2a296847
jacobvanetten (Dr ) <jacobvanetten@yahoo.com>
2013-06-07 21:23:06 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cropcc@69 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Try3/R/makeModel.R
+++ pkg/Try3/R/makeModel.R
@@ -15,35 +15,43 @@ makeModel <- function()
   nb1 <- gtable(items=myData, container=group1, expand=TRUE)
 
   nb2 <- ggroup(horizontal=FALSE, container=group1)
-  size(aa) <- c(150,150)
   
   cn <- as.data.frame(cbind(1:ncol(myData), colnames(myData)))
   colnames(cn) <- c("Column_number", "Variable_name")
   
   addSpace(nb2,10)
   glabel("Select the ranking (response) variables:",container=nb2)
-  aa <- gtable(cn, chosencol = 1, multiple=TRUE, container=nb2,
-                handler = function(h,...) .GlobalEnv$rankingVars <- svalue(h$obj))
+  aa <- gtable(cn, chosencol = 1, multiple=TRUE, container=nb2)
   
   size(aa) <- c(150,100)
   
   addSpace(nb2,10)
   glabel("Select the explanatory variables:",container=nb2)
-  bb <- gtable(cn, chosencol = 1, multiple=TRUE, container=nb2,
-         handler = function(h,...) .GlobalEnv$explVars <- svalue(h$obj))
+  bb <- gtable(cn, chosencol = 1, multiple=TRUE, container=nb2)
   
   size(bb) <- c(150,100)
   
   addSpace(nb2,10)
-  glabel("Select the question categories (if different aspects were evaluated):", container=nb2)
-  cc <- gtable(cn, chosencol = 1, multiple=TRUE, container=nb2,
-         handler = function(h,...) .GlobalEnv$questionVars <- svalue(h$obj))
+  glabel("Select variable representing the questions (if different aspects were evaluated):", container=nb2)
+  cc <- gtable(cn, chosencol = 1, multiple=TRUE, container=nb2)
   
   size(cc) <- c(150,100)
   
   nb3 <- ggroup(horizontal=TRUE, container=nb2)
   addSpring(nb3)
   b <- gbutton("Create model", container=nb3, handler = function(h, ...){
+
+    .GlobalEnv$rankingVars <- colnames(myData)[as.integer(svalue(aa))]
+    .GlobalEnv$explVars <- colnames(myData)[as.integer(svalue(bb))]
+    .GlobalEnv$questionVar <- colnames(myData)[as.integer(svalue(cc))]
+    
+    gmessage(paste("Model created. \nRanking variables:\n", 
+                   rankingVars,
+                   "\nExplanatory variables:\n",
+                   explVars,
+                   "\nQuestion (aspect evaluated) variable:\n",
+                   questionVar
+                   ), title="Done", icon="info")
     
   })
   
