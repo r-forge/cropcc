c03ab8887782bfba9951d911efec42253e62249c
jacobvanetten (Dr ) <jacobvanetten@yahoo.com>
2013-06-13 21:29:17 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cropcc@73 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Try3/R/makeModel.R
+++ pkg/Try3/R/makeModel.R
@@ -1,3 +1,35 @@
+.treeModel <- function(myData, itemsgiven, rankings, explanatory)
+{
+  
+  rankingsD <- myData[,rankings]
+  itemsgivenD <- myData[,itemsgiven]
+  explanatoryD <- myData[,explanatory]
+  
+  itemnames <- sort(unique(as.vector(unlist(itemsgivenD))))
+  
+  cc <- combn2(1:length(itemnames), 2)
+  pc <- matrix(NA, nrow=nrow(items), ncol= ncol(cc))
+  
+  for(i in 1:dim(itemsgivenD)[1])
+  {
+    
+    aa <- combn2(match(unlist(itemsgivenD[i,]), itemnames)[unlist(rankingsD[i,])], 2)
+    
+    for(j in 1:dim(aa)[2]){
+      pc[i, aa[1,j] == cc[1,] & aa[2,j] == cc[2,]] <- 1
+      pc[i, aa[2,j] == cc[1,] & aa[1,j] == cc[2,]] <- -1
+    }
+  }
+  
+  pc <- paircomp(pc, labels=itemnames)
+  
+  myData$pc <- pc
+  
+  v_tree <- bttree(formula(paste("pc", "~", paste(explanatory, collapse="+"))), data = myData, minsplit = 15, type="logit")
+  return(v_tree)
+  
+}
+
 makeModel <- function()
 {
   
@@ -7,6 +39,8 @@ makeModel <- function()
     return()
     
   }
+  
+  
   
   w2 <- gwindow(title="Try3 - Make model", visible=FALSE, parent=c(0,0)) #previous window for size...
   size(w2) <- c(600,600)
@@ -20,39 +54,67 @@ makeModel <- function()
   colnames(cn) <- c("Column_number", "Variable_name")
   
   addSpace(nb2,10)
-  glabel("Select the ranking (response) variables:",container=nb2)
+  glabel("Select the columns with the items given to each observer (original randomization):",container=nb2)
   aa <- gtable(cn, chosencol = 1, multiple=TRUE, container=nb2)
-  
   size(aa) <- c(150,100)
-  
+    
   addSpace(nb2,10)
-  glabel("Select the explanatory variables:",container=nb2)
+  glabel("Select the ranking (response) variables:",container=nb2)
   bb <- gtable(cn, chosencol = 1, multiple=TRUE, container=nb2)
-  
   size(bb) <- c(150,100)
   
   addSpace(nb2,10)
-  glabel("Select variable representing the questions (if different aspects were evaluated):", container=nb2)
+  glabel("Select the explanatory variables:",container=nb2)
   cc <- gtable(cn, chosencol = 1, multiple=TRUE, container=nb2)
-  
   size(cc) <- c(150,100)
+  
+  addSpace(nb2,10)
+  glabel("Select variable representing the questions (if different aspects were evaluated):", container=nb2)
+  dd <- gtable(cn, chosencol = 1, multiple=TRUE, container=nb2)
+  size(dd) <- c(150,100)
   
   nb3 <- ggroup(horizontal=TRUE, container=nb2)
   addSpring(nb3)
+  
   b <- gbutton("Create model", container=nb3, handler = function(h, ...){
 
-    .GlobalEnv$rankingVars <- colnames(myData)[as.integer(svalue(aa))]
-    .GlobalEnv$explVars <- colnames(myData)[as.integer(svalue(bb))]
-    .GlobalEnv$questionVar <- colnames(myData)[as.integer(svalue(cc))]
+    galert("Creating model... This can take some time.", parent=c(1,1), delay=4)
+        
+    .GlobalEnv$itemsgivenVars <- colnames(myData)[as.integer(svalue(aa))]
+    .GlobalEnv$rankingsVars <- colnames(myData)[as.integer(svalue(bb))]
+    .GlobalEnv$explanatoryVars <- colnames(myData)[as.integer(svalue(cc))]
+    .GlobalEnv$questionVar <- colnames(myData)[as.integer(svalue(dd))]
+
+    nq <- 1
+    if(length(questionVar>0)){
+      
+      questions <- sort(as.character(unique(myData[,questionVar])))
+      nq <- length(questions)
+      
+    }
     
-    gmessage(paste("Model created. \nRanking variables:\n", 
-                   rankingVars,
+    models <- vector(mode="list", length=nq)
+    
+    for(i in 1:nq)
+    {
+      
+      if(nq>1){myData_i <- myData[myData[,questionVar] == questions[i],]}
+      else{myData_i <- myData}
+      models[[i]] <- .treeModel(myData_i, itemsgivenVars, rankingsVars, explanatoryVars)
+      
+    }
+    
+    dispose(w2)
+    
+    gmessage(paste("Model created. \nItems given:\n", 
+                   paste(itemsgivenVars, collapse=", "),"\nRanking variables:\n", 
+                   paste(rankingsVars, collapse=", "),
                    "\nExplanatory variables:\n",
-                   explVars,
+                   paste(explanatoryVars, collapse=", "),
                    "\nQuestion (aspect evaluated) variable:\n",
-                   questionVar
+                   paste(questionVar, collapse=", ")
                    ), title="Done", icon="info")
-    
+        
   })
   
   visible(w2) <- TRUE
