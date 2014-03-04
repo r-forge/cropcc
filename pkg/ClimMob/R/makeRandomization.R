0073b3225938e52b45e75f2c08ad14b22a363d72
jacobvanetten (Dr ) <jacobvanetten@yahoo.com>
2014-03-04 02:45:58 +0000

    makeRandomization balancing implemented
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cropcc@150 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ClimMob/R/makeRandomization.R
+++ pkg/ClimMob/R/makeRandomization.R
@@ -73,28 +73,15 @@
       itemnames <- unlist(strsplit(svalue(setitemnames), "\n"))
       nobservers <- as.integer(svalue(setnobservers))
       identification <- svalue(setid)
-            
-      nvar <- length(itemnames)
       
-      #Input
-      varieties <- 1:nvar
+      #Input      
+
       codes <- matrix(nrow=sum(nobservers), ncol=nitems+1)
       colnames(codes) <- c(tl[18,la], paste(tl[19,la], 1:nitems, sep=""))
       codes[,1] <- paste(identification, 1:sum(nobservers), sep="")
       
-      varcombinations <- t((combn(varieties, 3)))
-      ncomb <- dim(varcombinations)[1]
-      n <- floor(nobservers/ncomb)
-      nfixed <- ncomb*n
-      
-      vars1 <- varcombinations[c(rep(1:(dim(varcombinations)[1]), times=n)),]
-      vars2 <- varcombinations[sample(ncomb, nobservers-nfixed),]
-      vars <- rbind(vars1, vars2)
-      
-      vars <- vars[sample(nobservers,nobservers),]
-      
-      for(i in 1:nobservers) codes[i,2:4] <- itemnames[sample(vars[i,], 3)]
-      
+      codes <- .rand2(codes, nitems, itemnames, nobservers, identification, tl)
+
       fn <- paste(identification, tl[16,la], sep="")
       
       if(svalue(decsep) == tl[9,la] & svalue(MSExcel) == tl[13,la]){write.csv(codes, fn, row.names=F, fileEncoding = "ASCII")}
@@ -126,3 +113,132 @@
   visible(w1) <- TRUE
   
 }
+
+#create randomization
+.rand <- function(codes, nitems, itemnames, nobservers, identification, tl)
+{
+  nvar <- length(itemnames)
+  varieties <- 1:nvar
+  
+  varcombinations <- t((combn(varieties, 3)))
+  ncomb <- dim(varcombinations)[1]
+  n <- floor(nobservers/ncomb)
+  nfixed <- ncomb*n
+  
+  vars1 <- varcombinations[c(rep(1:(dim(varcombinations)[1]), times=n)),]
+  vars2 <- varcombinations[sample(ncomb, nobservers-nfixed),]
+  vars <- rbind(vars1, vars2)
+  
+  #vars <- vars[sample(nobservers,nobservers),]
+  
+  rl <- max(2, round(nvar/nitems))
+  nl <- ceiling(nobservers/rl)
+  index <- rep(1:nl, each=rl)[1:nobservers]
+  
+  stopCrit <- FALSE
+  Index1 <- sample(nobservers,nobservers)
+  Index2 <- Index1
+  score2 <- 0
+  nperm1 <- nobservers
+  while(!stopCrit)
+  {
+    
+    nperm2 <- ceiling(nperm1)
+    Index1 <- Index2
+    s2 <- sample(nobservers, nperm2)
+    s1 <- s2[sample(nperm2, nperm2)]
+    Index1[s1] <- Index2[s2]
+    score1 <- .balanceFunction(Index1,vars,nvar,index)
+    if(score1 > score2) Index2 <- Index1
+    nperm1 <- nperm1 - 0.005
+    stopCrit <- nperm1 < 0.0055
+    
+  }
+  
+  vars <- vars[Index2,]
+  for(i in 1:nobservers) codes[i,2:(nitems+1)] <- itemnames[sample(vars[i,], nitems)]
+  return(codes)
+  
+}
+
+.balanceFunction <- function(Index, vars, nvar, index)
+{
+  
+  vars2 <- vars[Index,]
+  ni <- index[length(index)]
+  m <- matrix(0, nrow=nvar, ncol=ni)
+  for(i in 1:ni) {
+    
+    tb <- table(as.vector(vars2[index == i,]))
+    m[as.integer(names(tb)),i] <- as.vector(tb)
+  
+  }
+  balance <- -sum((m/sum(m))^2)
+  return(balance)
+  
+}
+
+.rand2 <- function(codes, nitems, itemnames, nobservers, identification, tl)
+{
+  nvar <- length(itemnames)
+  varieties <- 1:nvar    
+  
+  varcombinations <- t((combn(varieties, 3)))
+  ncomb <- dim(varcombinations)[1]
+  n <- floor(nobservers/ncomb)
+  nfixed <- ncomb*n
+  
+  vars1 <- varcombinations[c(rep(1:(dim(varcombinations)[1]), times=n)),]
+  vars2 <- varcombinations[sample(ncomb, nobservers-nfixed),]
+  vars <- rbind(vars1, vars2)
+  for(i in 1:nobservers) vars[i,] <- sample(vars[i,], nitems)
+  
+  varOrdered <- matrix(NA, nrow=nobservers, ncol=nitems)
+  ind <- sample(nobservers,1)
+  varOrdered[1,] <- vars[ind,]
+  vars <- vars[-ind,]
+  
+  .dist <- function(x) {
+    
+    x <- match(x,rev(vO))
+    x[is.na(x)] <- 6
+    Dist <- sum(ceiling(x/nitems)^1.7)
+    return(Dist)
+  
+  }
+  
+  for(i in 2:nobservers)
+  {
+  
+    vO <- as.vector(t(varOrdered[1:(i-1),]))
+    ranks <- as.vector(apply(vars, 1, .dist))
+    index <- which(ranks == max(ranks))
+    
+    #if length(index) > 1
+    
+    tb <- rep(0,times=nvar)
+    tb1 <- table(c(as.vector(vO)))
+    tb[as.integer(names(tb1))] <- as.vector(tb1)
+    
+    evenS <- vector(length=length(index))
+    
+    for(j in 1:length(index))
+    {
+      
+      tb2 <- tb
+      tb2[vars[index[j],]] <- tb2[vars[index[j],]] + 1
+      evenS[j] <- -sum(tb2^2)
+    
+    }
+    
+    index <- index[which(evenS == max(evenS))[1]]
+    varOrdered[i,] <- vars[index,]
+    vars <- vars[-index,,drop=FALSE]
+    
+  }
+  
+  codes[,2:(nitems+1)] <- itemnames[varOrdered]
+  return(codes)
+  
+} 
+  
